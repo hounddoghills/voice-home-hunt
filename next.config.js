d6debfcf9e22820a4166fd74ba8d57627a3797a2
hounddoghills <hounddoghills@gmail.com>
2026-02-10 21:27:25 -0600

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: ['photos.sparkapi.com', 'images.unsplash.com'],
+  },
+}
+
+module.exports = nextConfig
